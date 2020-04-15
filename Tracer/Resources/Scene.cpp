ca704a52881603a8cdd9a9774c6cad8cf0379de9
rdbruijne <rickb1988@gmail.com>
2020-04-16 01:29:39 +0200

    Null check on scene add

--- Tracer/Resources/Scene.cpp
+++ Tracer/Resources/Scene.cpp
@@ -44,7 +44,7 @@ namespace Tracer
 
 	void Scene::AddMaterial(std::shared_ptr<Material> material)
 	{
-		if(std::find(mMaterials.begin(), mMaterials.end(), material) == mMaterials.end())
+		if(material && std::find(mMaterials.begin(), mMaterials.end(), material) == mMaterials.end())
 		{
 			mMaterials.push_back(material);
 			mIsDirty = true;
@@ -55,7 +55,7 @@ namespace Tracer
 
 	void Scene::AddMesh(std::shared_ptr<Mesh> mesh)
 	{
-		if(std::find(mMeshes.begin(), mMeshes.end(), mesh) == mMeshes.end())
+		if(mesh && std::find(mMeshes.begin(), mMeshes.end(), mesh) == mMeshes.end())
 		{
 			mMeshes.push_back(mesh);
 			mIsDirty = true;
@@ -66,7 +66,7 @@ namespace Tracer
 
 	void Scene::AddModel(std::shared_ptr<Model> model)
 	{
-		if(std::find(mModels.begin(), mModels.end(), model) == mModels.end())
+		if(model && std::find(mModels.begin(), mModels.end(), model) == mModels.end())
 		{
 			mModels.push_back(model);
 			mIsDirty = true;
