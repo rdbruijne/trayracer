7f83ca992037e34503de38b9aafcebf16c9b374a
rdbruijne <rickb1988@gmail.com>
2020-05-23 22:27:49 +0200

    JSON read/write helper functions

--- Tracer/FileIO/SceneFile.cpp
+++ Tracer/FileIO/SceneFile.cpp
@@ -25,25 +25,188 @@
 
 // #TODO: clean code by adding helper functions in nameless namespace
 
+using namespace rapidjson;
+
 namespace Tracer
 {
+	namespace
+	{
+		//----------------------------------------------------------------------------------------------------------------------
+		// read helpers
+		//----------------------------------------------------------------------------------------------------------------------
+		bool Read(const Value& jsonValue, const char* memberName, float& result)
+		{
+			if(!jsonValue.HasMember(memberName))
+				return false;
+
+			const Value& val = jsonValue[memberName];
+			if(!val.IsNumber())
+				return false;
+
+			result = val.GetFloat();
+			return true;
+		}
+
+
+
+		bool Read(const Value& jsonValue, const char* memberName, int& result)
+		{
+			if(!jsonValue.HasMember(memberName))
+				return false;
+
+			const Value& val = jsonValue[memberName];
+			if(!val.IsNumber())
+				return false;
+
+			result = val.GetInt();
+			return true;
+		}
+
+
+
+		bool Read(const Value& jsonValue, const char* memberName, float3& result)
+		{
+			if(!jsonValue.HasMember(memberName))
+				return false;
+
+			const Value& val = jsonValue[memberName];
+			if(!val.IsArray() || val.Size() != 3 || !val[0].IsNumber() || !val[1].IsNumber() || !val[2].IsNumber())
+				return false;
+
+			result = make_float3(val[0].GetFloat(), val[1].GetFloat(), val[2].GetFloat());
+			return true;
+		}
+
+
+
+		bool Read(const Value& jsonValue, const char* memberName, float3x4& result)
+		{
+			if(!jsonValue.HasMember(memberName))
+				return false;
+
+			const Value& val = jsonValue[memberName];
+			if(!val.IsArray() || val.Size() != 12)
+				return false;
+
+			float m[12] = {};
+			for(SizeType i = 0; i < val.Size(); i++)
+			{
+				if(!val[i].IsNumber())
+					return false;
+				m[i] = val[i].GetFloat();
+			}
+
+			result = make_float3x4(m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10], m[11]);
+			return true;
+		}
+
+
+		bool Read(const Value& jsonValue, const char* memberName, std::string& result)
+		{
+			if(!jsonValue.HasMember(memberName))
+				return false;
+
+			const Value& val = jsonValue[memberName];
+			if(!val.IsString())
+				return false;
+
+			result = val.GetString();
+			return true;
+		}
+
+
+
+		//----------------------------------------------------------------------------------------------------------------------
+		// write helpers
+		//----------------------------------------------------------------------------------------------------------------------
+		void Write(Value& jsonValue, Document::AllocatorType& allocator, const char* memberName, float val)
+		{
+			Value v = Value(kObjectType);
+			v.SetFloat(val);
+			jsonValue.AddMember(StringRef(memberName), v, allocator);
+		}
+
+
+
+		void Write(Value& jsonValue, Document::AllocatorType& allocator, const char* memberName, int val)
+		{
+			Value v = Value(kObjectType);
+			v.SetInt(val);
+			jsonValue.AddMember(StringRef(memberName), v, allocator);
+		}
+
+
+
+		void Write(Value& jsonValue, Document::AllocatorType& allocator, const char* memberName, float3 val)
+		{
+			Value jsonVector = Value(kArrayType);
+			jsonVector.PushBack(val.x, allocator);
+			jsonVector.PushBack(val.y, allocator);
+			jsonVector.PushBack(val.z, allocator);
+
+			Value jsonVectorObj = Value(kObjectType);
+			jsonVectorObj.AddMember(StringRef(memberName), jsonVector, allocator);
+		}
+
+
+
+		void Write(Value& jsonValue, Document::AllocatorType& allocator, const char* memberName, float3x4 val)
+		{
+			const float tempVals[12] =
+			{
+				val.x.x, val.x.y, val.x.z,
+				val.y.x, val.y.y, val.y.z,
+				val.z.x, val.z.y, val.z.z,
+				val.tx, val.ty, val.tz
+			};
+
+			Value jsonMatrix = Value(kArrayType);
+			for(int i = 0; i < 12; i++)
+			{
+				Value f = Value(kObjectType);
+				f.SetFloat(tempVals[i]);
+				jsonMatrix.PushBack(f, allocator);
+			}
+
+			Value jsonMatrixObj = Value(kObjectType);
+			jsonValue.AddMember(StringRef(memberName), jsonMatrix, allocator);
+		}
+
+
+
+		void Write(Value& jsonValue, Document::AllocatorType& allocator, const char* memberName, const std::string& val)
+		{
+			Value v = Value(kObjectType);
+			v.SetString(val.c_str(), static_cast<SizeType>(val.length()), allocator);
+			jsonValue.AddMember(StringRef(memberName), v, allocator);
+		}
+	}
+
+
+
 	void SceneFile::Load(const std::string& sceneFile, Scene* scene, CameraNode* camNode, Renderer* renderer, Window* window)
 	{
-		using namespace rapidjson;
-
 		printf("Loading scene from \"%s\"\n", sceneFile.c_str());
 
 		// read file from disk
-		std::ifstream f(sceneFile);
-		IStreamWrapper isw(f);
 		Document doc;
-		doc.ParseStream(isw);
-		f.close();
-		if(!doc.IsObject())
 		{
-			printf("\"%s\" does not contain valid json code.", sceneFile.c_str());
-			return;
+			std::ifstream f(sceneFile);
+			IStreamWrapper isw(f);
+			doc.ParseStream(isw);
+			f.close();
+			if(!doc.IsObject())
+			{
+				printf("\"%s\" does not contain valid json code.", sceneFile.c_str());
+				return;
+			}
 		}
+
+		// reusable data for json reading
+		int i;
+		float f;
+		float3 f3;
+		float3x4 f3x4;
 
 		// models/instances
 		if(scene)
@@ -59,72 +222,39 @@ namespace Tracer
 					{
 						const Value& jsonInstance = jsonModels[modelIx];
 
-						// check for required fields
-						if(!jsonInstance.HasMember("model"))
-							continue;
-
 						// model path
-						const std::string modelPath = jsonInstance["model"].GetString();
+						std::string modelPath;
+						if(!Read(jsonInstance, "model", modelPath))
+							continue;
 
 						// transform
 						float3x4 modelTransform = make_float3x4(); // identity
 						if(jsonInstance.HasMember("transform"))
 						{
 							const Value& jsonTransformOperations = jsonInstance["transform"];
-							for(rapidjson::SizeType transOpIx = 0; transOpIx < jsonTransformOperations.Size(); transOpIx++)
+							for(SizeType transOpIx = 0; transOpIx < jsonTransformOperations.Size(); transOpIx++)
 							{
 								const Value& op = jsonTransformOperations[transOpIx];
-								if(op.HasMember("matrix"))
-								{
-									const Value& opVal = op["matrix"];
-									if(opVal.IsArray() && opVal.Size() == 12)
-									{
-										float transValues[12] = {};
-										for(SizeType i = 0; i < opVal.Size(); i++)
-											transValues[i] = opVal[i].GetFloat();
-										const float3x4 t = make_float3x4(
-											transValues[0], transValues[1], transValues[ 2], transValues[ 3],
-											transValues[4], transValues[5], transValues[ 6], transValues[ 7],
-											transValues[8], transValues[9], transValues[10], transValues[11]);
-										modelTransform *= t;
-									}
-								}
-								else if(op.HasMember("translate"))
-								{
-									const Value& opVal = op["translate"];
-									if(opVal.IsArray() && opVal.Size() == 3)
-										modelTransform *= translate_3x4(opVal[0].GetFloat(), opVal[1].GetFloat(), opVal[2].GetFloat());
-								}
-								else if(op.HasMember("scale"))
-								{
-									const Value& opVal = op["scale"];
-									if(opVal.IsArray() && opVal.Size() == 3)
-										modelTransform *= scale_3x4(opVal[0].GetFloat(), opVal[1].GetFloat(), opVal[2].GetFloat());
-									else if(opVal.IsNumber())
-										modelTransform *= scale_3x4(opVal.GetFloat());
-								}
-								else if(op.HasMember("rotate-x"))
-								{
-									const Value& opVal = op["rotate-x"];
-									modelTransform *= rotate_x_3x4(opVal.GetFloat() * DegToRad);
-								}
-								else if(op.HasMember("rotate-y"))
-								{
-									const Value& opVal = op["rotate-y"];
-									modelTransform *= rotate_y_3x4(opVal.GetFloat() * DegToRad);
-								}
-								else if(op.HasMember("rotate-z"))
-								{
-									const Value& opVal = op["rotate-z"];
-									modelTransform *= rotate_z_3x4(opVal.GetFloat() * DegToRad);
-								}
+								if(Read(op, "matrix", f3x4))
+									modelTransform *= f3x4;
+								else if(Read(op, "translate", f3))
+									modelTransform *= translate_3x4(f3);
+								else if(Read(op, "rotate-x", f))
+									modelTransform *= rotate_x_3x4(f * DegToRad);
+								else if(Read(op, "rotate-y", f))
+									modelTransform *= rotate_y_3x4(f * DegToRad);
+								else if(Read(op, "rotate-z", f))
+									modelTransform *= rotate_z_3x4(f * DegToRad);
+								else if(Read(op, "scale", f))
+									modelTransform *= scale_3x4(f);
+								else if(Read(op, "scale", f3))
+									modelTransform *= scale_3x4(f3);
 							}
 						}
 
 						// instance name
 						std::string instName = FileNameWithoutExtension(modelPath);
-						if(jsonInstance.HasMember("name"))
-							instName = jsonInstance["name"].GetString();
+						Read(jsonInstance, "name", instName);
 
 						// find model for given path
 						std::shared_ptr<Model> model = nullptr;
@@ -156,59 +286,16 @@ namespace Tracer
 			float fov = 90.f;
 
 			const Value& jsonCamera = doc["camera"];
+			Read(jsonCamera, "position", pos);
+			Read(jsonCamera, "target", target);
+			Read(jsonCamera, "up", up);
+			Read(jsonCamera, "fov", fov);
 
-			// position
-			if(jsonCamera.HasMember("position"))
-			{
-				const Value& jsonPos = jsonCamera["position"];
-				if(jsonPos.IsArray() && jsonPos.Size() == 3)
-				{
-					pos.x = jsonPos[0].GetFloat();
-					pos.y = jsonPos[1].GetFloat();
-					pos.z = jsonPos[2].GetFloat();
-				}
-			}
-
-			// target
-			if(jsonCamera.HasMember("target"))
-			{
-				const Value& jsonTarget = jsonCamera["target"];
-				if(jsonTarget.IsArray() && jsonTarget.Size() == 3)
-				{
-					target.x = jsonTarget[0].GetFloat();
-					target.y = jsonTarget[1].GetFloat();
-					target.z = jsonTarget[2].GetFloat();
-				}
-			}
-
-			// up
-			if(jsonCamera.HasMember("position"))
-			{
-				const Value& jsonUp = jsonCamera["up"];
-				if(jsonUp.IsArray() && jsonUp.Size() == 3)
-				{
-					up.x = jsonUp[0].GetFloat();
-					up.y = jsonUp[1].GetFloat();
-					up.z = jsonUp[2].GetFloat();
-				}
-			}
-
-			// fov
-			if(jsonCamera.HasMember("fov"))
-			{
-				fov = jsonCamera["fov"].GetFloat();
-				if(fov < .1f || fov >= 179.9f)
-				{
-					printf("FOV must be between 0 and 180, correcting.");
-					fov = fminf(fmaxf(fov, .1f), 179.9f);
-				}
-			}
+			// corrections
+			fov = fminf(fmaxf(fov, .1f), 179.9f);
 
 			if(pos == target)
-			{
-				printf("Camera position & target cannot overlap, correcting.");
 				target = pos + make_float3(0, 0, 1);
-			}
 
 			// set camera
 			*camNode = CameraNode(pos, target, up, fov * DegToRad);
@@ -218,27 +305,16 @@ namespace Tracer
 		if(renderer && doc.HasMember("renderer"))
 		{
 			const Value& jsonRenderer = doc["renderer"];
-
-			// multi-sample
-			if(jsonRenderer.HasMember("multisample"))
-				renderer->SetMultiSample(jsonRenderer["multisample"].GetInt());
-
-			// max depth
-			if(jsonRenderer.HasMember("maxdepth"))
-				renderer->SetMaxDepth(jsonRenderer["maxdepth"].GetInt());
-
-			// ao dist
-			if(jsonRenderer.HasMember("aodist"))
-				renderer->SetAODist(jsonRenderer["aodist"].GetFloat());
-
-			// z-depth max
-			if(jsonRenderer.HasMember("zdepthmax"))
-				renderer->SetZDepthMax(jsonRenderer["zdepthmax"].GetFloat());
-
-			// sky color
-			const Value& jsonSkyColor = jsonRenderer["skycolor"];
-			if(jsonSkyColor.IsArray() && jsonSkyColor.Size() == 3)
-				renderer->SetSkyColor(make_float3(jsonSkyColor[0].GetFloat(), jsonSkyColor[1].GetFloat(), jsonSkyColor[2].GetFloat()));
+			if(Read(jsonRenderer, "multisample", i))
+				renderer->SetMultiSample(i);
+			if(Read(jsonRenderer, "maxdepth", i))
+				renderer->SetMaxDepth(i);
+			if(Read(jsonRenderer, "aodist", f))
+				renderer->SetAODist(f);
+			if(Read(jsonRenderer, "zdepthmax", f))
+				renderer->SetZDepthMax(f);
+			if(Read(jsonRenderer, "skycolor", f3))
+				renderer->SetSkyColor(f3);
 		}
 
 		// post
@@ -247,13 +323,8 @@ namespace Tracer
 			Window::ShaderProperties postProps = window->PostShaderProperties();
 			const Value& jsonPost = doc["post"];
 
-			// exposure
-			if(jsonPost.HasMember("exposure"))
-				postProps.exposure = jsonPost["exposure"].GetFloat();
-
-			// gamma
-			if(jsonPost.HasMember("gamma"))
-				postProps.gamma = jsonPost["gamma"].GetFloat();
+			Read(jsonPost, "exposure", postProps.exposure);
+			Read(jsonPost, "gamma", postProps.gamma);
 
 			window->SetPostShaderProperties(postProps);
 		}
@@ -263,8 +334,6 @@ namespace Tracer
 
 	void SceneFile::Save(const std::string& sceneFile, Scene* scene, CameraNode* camNode, Renderer* renderer, Window* window)
 	{
-		using namespace rapidjson;
-
 		printf("Saving scene to \"%s\"\n", sceneFile.c_str());
 
 		// create json document
@@ -287,37 +356,11 @@ namespace Tracer
 
 					// instance
 					Value jsonInst = Value(kObjectType);
-
-					// name
-					Value jsonName = Value(kObjectType);
-					const std::string instName = inst->Name();
-					jsonName.SetString(instName.c_str(), static_cast<SizeType>(instName.length()), allocator);
-					jsonInst.AddMember("name", jsonName, allocator);
-
-					// file path
-					Value jsonPath = Value(kObjectType);
-					const std::string filePath = inst->GetModel()->FilePath();
-					jsonPath.SetString(filePath.c_str(), static_cast<SizeType>(filePath.length()), allocator);
-					jsonInst.AddMember("model", jsonPath, allocator);
-
-					// transform
-					Value jsonMatrix = Value(kArrayType);
-					const float3x4& t = inst->Transform();
-					float tempVals[12] = { t.x.x, t.x.y, t.x.z, t.y.x, t.y.y, t.y.z, t.z.x, t.z.y, t.z.z, t.tx, t.ty, t.tz };
-					for(int i = 0; i < 12; i++)
-					{
-						Value f = Value(kObjectType);
-						f.SetFloat(tempVals[i]);
-						jsonMatrix.PushBack(f, allocator);
-					}
-
-					Value jsonMatrixObj = Value(kObjectType);
-					jsonMatrixObj.AddMember("matrix", jsonMatrix, allocator);
+					Write(jsonInst, allocator, "name", inst->Name());
+					Write(jsonInst, allocator, "model", inst->GetModel()->FilePath());
 
 					Value jsonTransform = Value(kArrayType);
-					jsonTransform.PushBack(jsonMatrixObj, allocator);
-
-					jsonInst.AddMember("transform", jsonTransform, allocator);
+					Write(jsonTransform, allocator, "matrix", inst->Transform());
 
 					// add model to array
 					jsonModels.PushBack(jsonInst, allocator);
@@ -329,120 +372,35 @@ namespace Tracer
 		// camera
 		if(camNode)
 		{
-			Value x = Value(kObjectType);
-			Value y = Value(kObjectType);
-			Value z = Value(kObjectType);
-
-			// position
-			Value jsonPos = Value(kArrayType);
-			const float3& camPos = camNode->Position();
-			x.SetFloat(camPos.x);
-			y.SetFloat(camPos.y);
-			z.SetFloat(camPos.z);
-			jsonPos.PushBack(x, allocator);
-			jsonPos.PushBack(y, allocator);
-			jsonPos.PushBack(z, allocator);
-
-			// target
-			Value jsonTarget = Value(kArrayType);
-			const float3& camTarget = camNode->Target();
-			x.SetFloat(camTarget.x);
-			y.SetFloat(camTarget.y);
-			z.SetFloat(camTarget.z);
-			jsonTarget.PushBack(x, allocator);
-			jsonTarget.PushBack(y, allocator);
-			jsonTarget.PushBack(z, allocator);
-
-			// up
-			Value jsonUp = Value(kArrayType);
-			const float3& camUp = camNode->Up();
-			x.SetFloat(camUp.x);
-			y.SetFloat(camUp.y);
-			z.SetFloat(camUp.z);
-			jsonUp.PushBack(x, allocator);
-			jsonUp.PushBack(y, allocator);
-			jsonUp.PushBack(z, allocator);
-
-			// fov
-			Value jsonFov = Value(kObjectType);
-			jsonFov.SetFloat(camNode->Fov() * RadToDeg);
-
-			// create json camera
 			Value jsonCam = Value(kObjectType);
-			jsonCam.AddMember("position", jsonPos, allocator);
-			jsonCam.AddMember("target", jsonTarget, allocator);
-			jsonCam.AddMember("up", jsonUp, allocator);
-			jsonCam.AddMember("fov", jsonFov, allocator);
-
-			// add to doc
+			Write(jsonCam, allocator, "position", camNode->Position());
+			Write(jsonCam, allocator, "target", camNode->Target());
+			Write(jsonCam, allocator, "up", camNode->Up());
+			Write(jsonCam, allocator, "fov", camNode->Fov());
 			doc.AddMember("camera", jsonCam, allocator);
 		}
 
 		// render settings
 		if(renderer)
 		{
-			Value jsonRenderSettings = Value(kObjectType);
-
-			// multi-sample
-			Value jsonMultiSample = Value(kObjectType);
-			jsonMultiSample.SetInt(renderer->MultiSample());
-			jsonRenderSettings.AddMember("multisample", jsonMultiSample, allocator);
-
-			// max depth
-			Value jsonMaxDepth = Value(kObjectType);
-			jsonMaxDepth.SetInt(renderer->MaxDepth());
-			jsonRenderSettings.AddMember("maxdepth", jsonMaxDepth, allocator);
-
-			// ao dist
-			Value jsonAoDist = Value(kObjectType);
-			jsonAoDist.SetFloat(renderer->AODist());
-			jsonRenderSettings.AddMember("aodist", jsonAoDist, allocator);
-
-			// z-depth max
-			Value jsonZDepthMax = Value(kObjectType);
-			jsonZDepthMax.SetFloat(renderer->ZDepthMax());
-			jsonRenderSettings.AddMember("zdepthmax", jsonZDepthMax, allocator);
-
-			// sky color
-			const float3& skyColor = renderer->SkyColor();
-
-			Value jsonSkyColorR = Value(kObjectType);
-			jsonSkyColorR.SetFloat(skyColor.x);
-
-			Value jsonSkyColorG = Value(kObjectType);
-			jsonSkyColorG.SetFloat(skyColor.y);
-
-			Value jsonSkyColorB = Value(kObjectType);
-			jsonSkyColorB.SetFloat(skyColor.z);
-
-			Value jsonSkyColor = Value(kArrayType);
-			jsonSkyColor.PushBack(jsonSkyColorR, allocator);
-			jsonSkyColor.PushBack(jsonSkyColorG, allocator);
-			jsonSkyColor.PushBack(jsonSkyColorB, allocator);
-			jsonRenderSettings.AddMember("skycolor", jsonSkyColor, allocator);
-
-			// add to doc
-			doc.AddMember("renderer", jsonRenderSettings, allocator);
+			Value jsonRenderer = Value(kObjectType);
+			Write(jsonRenderer, allocator, "multisample", renderer->MultiSample());
+			Write(jsonRenderer, allocator, "maxdepth", renderer->MaxDepth());
+			Write(jsonRenderer, allocator, "aodist", renderer->AODist());
+			Write(jsonRenderer, allocator, "zdepthmax", renderer->ZDepthMax());
+			Write(jsonRenderer, allocator, "skycolor", renderer->SkyColor());
+			doc.AddMember("renderer", jsonRenderer, allocator);
 		}
 
 		// post settings
 		if(window)
 		{
 			const Window::ShaderProperties& postProps = window->PostShaderProperties();
-			Value jsonPostSettings = Value(kObjectType);
 
-			// exposure
-			Value jsonExposure = Value(kObjectType);
-			jsonExposure.SetFloat(postProps.exposure);
-			jsonPostSettings.AddMember("exposure", jsonExposure, allocator);
-
-			// gamma
-			Value jsonGamma = Value(kObjectType);
-			jsonGamma.SetFloat(postProps.gamma);
-			jsonPostSettings.AddMember("gamma", jsonGamma, allocator);
-
-			// add to doc
-			doc.AddMember("post", jsonPostSettings, allocator);
+			Value jsonPost = Value(kObjectType);
+			Write(jsonPost, allocator, "exposure", postProps.exposure);
+			Write(jsonPost, allocator, "gamma", postProps.gamma);
+			doc.AddMember("post", jsonPost, allocator);
 		}
 
 		// write to disk
