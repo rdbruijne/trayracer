875a42f2f638fec2d2081a6bae61c559cd8f6d22
rdbruijne <rickb1988@gmail.com>
2020-08-17 20:14:16 +0200

    Added option for graphs in StatGui

--- Tracer/GUI/StatGui.cpp
+++ Tracer/GUI/StatGui.cpp
@@ -8,6 +8,8 @@
 // ImGUI
 #include "imgui/imgui.h"
 
+#define USE_GRAPHS	false
+
 namespace Tracer
 {
 	namespace
@@ -30,17 +32,45 @@ namespace Tracer
 
 	void StatGui::DrawImpl()
 	{
+#if USE_GRAPHS
+#define SPACE				\
+	ImGui::Spacing();		\
+	ImGui::NextColumn();	\
+	ImGui::Spacing();		\
+	ImGui::NextColumn();	\
+	ImGui::Separator();		\
+	ImGui::Spacing();		\
+	ImGui::NextColumn();	\
+	ImGui::Spacing();		\
+	ImGui::NextColumn();
+#else
 #define SPACE						\
 	for(int i = 0; i < 4; i++)		\
 	{								\
 		ImGui::Spacing();			\
 		ImGui::NextColumn();		\
 	}
+#endif
 
 #define ROW(s, ...)				\
 	ImGui::Text(s);				\
 	ImGui::NextColumn();		\
 	ImGui::Text(__VA_ARGS__);	\
+	ImGui::NextColumn();
+
+#define GRAPH(arr, s, ...)							\
+	ImGui::Text(s);									\
+	ImGui::NextColumn();							\
+	ImGui::Text(__VA_ARGS__);						\
+	ImGui::NextColumn();							\
+	ImGui::NextColumn();							\
+	ImGui::PushItemWidth(-1);						\
+	ImGui::PlotLines("", arr.data(),				\
+		static_cast<int>(msGraphSize),				\
+		static_cast<int>(mGraphIx),					\
+		nullptr, 0,									\
+		*std::max_element(arr.begin(), arr.end()));	\
+	ImGui::PopItemWidth();							\
 	ImGui::NextColumn();
 
 		ImGui::Begin("Statistics", &mEnabled);
@@ -51,15 +81,41 @@ namespace Tracer
 		}
 		else
 		{
-			ImGui::Columns(2);
-
 			// fetch stats
 			const Renderer::RenderStats renderStats = GuiHelpers::renderer->Statistics();
 
+#if USE_GRAPHS
+			// update graph times
+			mFramerates[mGraphIx]         = 1e3f / mFrameTimeMs;
+			mFrameTimes[mGraphIx]         = mFrameTimeMs;
+			mBuildTimes[mGraphIx]         = mBuildTimeMs;
+			mPrimaryPathTimes[mGraphIx]   = renderStats.primaryPathTimeMs;
+			mSecondaryPathTimes[mGraphIx] = renderStats.secondaryPathTimeMs;
+			mDeepPathTimes[mGraphIx]      = renderStats.deepPathTimeMs;
+			mShadowTimes[mGraphIx]        = renderStats.shadowTimeMs;
+			mShadeTimes[mGraphIx]         = renderStats.shadeTimeMs;
+			mDenoiseTimes[mGraphIx]       = renderStats.denoiseTimeMs;
+
+			// update pathcounts
+			mPathCounts[mGraphIx]          = PerSec(renderStats.pathCount, mFrameTimeMs) * 1e-6f;
+			mPrimaryPathCounts[mGraphIx]   = PerSec(renderStats.primaryPathCount, mFrameTimeMs) * 1e-6f;
+			mSecondaryPathCounts[mGraphIx] = PerSec(renderStats.secondaryPathCount, mFrameTimeMs) * 1e-6f;
+			mDeepPathCounts[mGraphIx]      = PerSec(renderStats.deepPathCount, mFrameTimeMs) * 1e-6f;
+			mShadowRayCounts[mGraphIx]     = PerSec(renderStats.shadowRayCount, mFrameTimeMs) * 1e-6f;
+
+			// increment graph ix
+			mGraphIx = (mGraphIx + 1) % msGraphSize;
+#endif
+
+			// init column layout
+			ImGui::Columns(2);
+
 			// table header
-			ImGui::Separator();
+#if false
 			ROW("Stat", "Value");
 			ImGui::Separator();
+			ImGui::Separator();
+#endif
 
 			// device
 			auto devProps = GuiHelpers::renderer->CudaDeviceProperties();
@@ -73,6 +129,17 @@ namespace Tracer
 			SPACE;
 
 			// times
+#if USE_GRAPHS
+			GRAPH(mFramerates, "FPS", "%.1f", 1e3f / mFrameTimeMs);
+			GRAPH(mFrameTimes, "Frame time", "%.1f ms", mFrameTimeMs);
+			GRAPH(mBuildTimes, "Scene build", "%.1f ms", mBuildTimeMs);
+			GRAPH(mPrimaryPathTimes, "Primary rays", "%.1f ms", renderStats.primaryPathTimeMs);
+			GRAPH(mSecondaryPathTimes, "Secondary rays", "%.1f ms", renderStats.secondaryPathTimeMs);
+			GRAPH(mDeepPathTimes, "Deep rays", "%.1f ms", renderStats.deepPathTimeMs);
+			GRAPH(mShadowTimes, "Shadow rays", "%.1f ms", renderStats.shadowTimeMs);
+			GRAPH(mShadeTimes, "Shade time", "%.1f ms", renderStats.shadeTimeMs);
+			GRAPH(mDenoiseTimes, "Denoise time", "%.1f ms", renderStats.denoiseTimeMs);
+#else
 			ROW("FPS", "%.1f", 1e3f / mFrameTimeMs);
 			ROW("Frame time", "%.1f ms", mFrameTimeMs);
 			ROW("Scene build", "%.1f ms", mBuildTimeMs);
@@ -82,15 +149,24 @@ namespace Tracer
 			ROW("Shadow rays", "%.1f ms", renderStats.shadowTimeMs);
 			ROW("Shade time", "%.1f ms", renderStats.shadeTimeMs);
 			ROW("Denoise time", "%.1f ms", renderStats.denoiseTimeMs);
+#endif
 
 			SPACE;
 
 			// rays
+#if USE_GRAPHS
+			GRAPH(mPathCounts, "Rays", "%.1f M (%.1f M/s)", renderStats.pathCount * 1e-6, PerSec(renderStats.pathCount, mFrameTimeMs) * 1e-6);
+			GRAPH(mPrimaryPathCounts, "Primaries", "%.1f M (%.1f M/s)", renderStats.primaryPathCount * 1e-6, PerSec(renderStats.primaryPathCount, renderStats.primaryPathTimeMs) * 1e-6);
+			GRAPH(mSecondaryPathCounts, "Secondaries", "%.1f M (%.1f M/s)", renderStats.secondaryPathCount * 1e-6f, PerSec(renderStats.secondaryPathCount, renderStats.secondaryPathTimeMs) * 1e-6f);
+			GRAPH(mDeepPathCounts, "Deep", "%.1f M (%.1f M/s)", renderStats.deepPathCount * 1e-6f, PerSec(renderStats.deepPathCount, renderStats.deepPathTimeMs) * 1e-6f);
+			GRAPH(mShadowRayCounts, "Shadow", "%.1f M (%.1f M/s)", renderStats.shadowRayCount * 1e-6f, PerSec(renderStats.shadowRayCount, renderStats.shadowTimeMs) * 1e-6f);
+#else
 			ROW("Rays", "%.1f M (%.1f M/s)", renderStats.pathCount * 1e-6, PerSec(renderStats.pathCount, mFrameTimeMs) * 1e-6);
 			ROW("Primaries", "%.1f M (%.1f M/s)", renderStats.primaryPathCount * 1e-6, PerSec(renderStats.primaryPathCount, renderStats.primaryPathTimeMs) * 1e-6);
 			ROW("Secondaries", "%.1f M (%.1f M/s)", renderStats.secondaryPathCount * 1e-6, PerSec(renderStats.secondaryPathCount, renderStats.secondaryPathTimeMs) * 1e-6);
 			ROW("Deep", "%.1f M (%.1f M/s)", renderStats.deepPathCount * 1e-6, PerSec(renderStats.deepPathCount, renderStats.deepPathTimeMs) * 1e-6);
 			ROW("Shadow", "%.1f M (%.1f M/s)", renderStats.shadowRayCount * 1e-6, PerSec(renderStats.shadowRayCount, renderStats.shadowTimeMs) * 1e-6);
+#endif
 
 			SPACE;
 
@@ -101,6 +177,8 @@ namespace Tracer
 			ROW("Unique triangle count", "%s", ThousandSeparators(GuiHelpers::scene->UniqueTriangleCount()).c_str());
 			ROW("Lights", "%s", ThousandSeparators(GuiHelpers::scene->LightCount()).c_str());
 			ROW("Unique lights", "%s", ThousandSeparators(GuiHelpers::scene->UniqueLightCount()).c_str());
+
+			ImGui::Columns();
 
 #undef SPACE
 #undef ROW
--- Tracer/GUI/StatGui.h
+++ Tracer/GUI/StatGui.h
@@ -4,6 +4,7 @@
 #include "BaseGui.h"
 
 // C++
+#include <array>
 #include <stdint.h>
 
 namespace Tracer
@@ -20,5 +21,25 @@ namespace Tracer
 
 	private:
 		void DrawImpl() final;
+
+		static constexpr size_t msGraphSize = 128;
+
+		size_t mGraphIx = 0;
+
+		std::array<float, msGraphSize> mFramerates;
+		std::array<float, msGraphSize> mFrameTimes;
+		std::array<float, msGraphSize> mBuildTimes;
+		std::array<float, msGraphSize> mPrimaryPathTimes;
+		std::array<float, msGraphSize> mSecondaryPathTimes;
+		std::array<float, msGraphSize> mDeepPathTimes;
+		std::array<float, msGraphSize> mShadowTimes;
+		std::array<float, msGraphSize> mShadeTimes;
+		std::array<float, msGraphSize> mDenoiseTimes;
+
+		std::array<float, msGraphSize> mPathCounts;
+		std::array<float, msGraphSize> mPrimaryPathCounts;
+		std::array<float, msGraphSize> mSecondaryPathCounts;
+		std::array<float, msGraphSize> mDeepPathCounts;
+		std::array<float, msGraphSize> mShadowRayCounts;
 	};
 }
