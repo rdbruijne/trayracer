8fe1b6f0915fd8615ff813feb58e1b67f118fee5
rdbruijne <rickb1988@gmail.com>
2023-07-03 11:58:41 +0200

    Macros for additional enum class operators

--- Tracer/Utility/Enum.h
+++ Tracer/Utility/Enum.h
@@ -2,51 +2,197 @@
 
 #include <type_traits>
 
-// declare trait type so that we can instantiate operators only for desired enums
+// Enum bitwise operators
 template<typename enum_type>
-struct enum_traits
-{};
+struct enum_bit_traits {};
 
-// use this to enable bitwise operators on enum classes
-#define ENUM_BITWISE_OPERATORS(E)               \
-    template<>                                  \
-    struct enum_traits<E>                       \
-    {                                           \
-        static constexpr int is_bitfield = 1;   \
-    };
+#define ENUM_BITWISE_OPERATORS(E)					\
+	template<>										\
+	struct enum_bit_traits<E>						\
+	{												\
+		static constexpr bool is_bitfield = true;	\
+	};
 
-// define template declaration to reduce code duplication
-#define _BIT_ENUM_T                                                         \
-    template<                                                               \
-        typename E, std::enable_if_t<enum_traits<E>::is_bitfield, int> = 0, \
-        typename T = std::underlying_type_t<E>>
 
+
+// Enum comparison operators
+template<typename enum_type>
+struct enum_compare_traits {};
+
+#define ENUM_COMPARE_OPERATORS(E)					\
+	template<>										\
+	struct enum_compare_traits<E>					\
+	{												\
+		static constexpr bool has_compare = true;	\
+	};
+
+
+
+// Enum arithmetic operators
+template<typename enum_type>
+struct enum_arithmetic_traits {};
+
+#define ENUM_ARITHMETIC_OPERATORS(E)				\
+	template<>										\
+	struct enum_arithmetic_traits<E>				\
+	{												\
+		static constexpr bool is_arithmetic = true;	\
+	};
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+// template declarations
+//------------------------------------------------------------------------------------------------------------------------------
+#define _ENUM_T(constraint)	template<typename E, typename T = std::underlying_type_t<E>, std::enable_if_t<constraint, bool> = false>
+#define _BIT_ENUM_T			_ENUM_T(enum_bit_traits<E>::is_bitfield)
+#define _CMP_ENUM_T			_ENUM_T(enum_compare_traits<E>::has_compare)
+#define _ARIT_ENUM_T		_ENUM_T(enum_arithmetic_traits<E>::is_arithmetic)
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+// Bitwise operators
+//------------------------------------------------------------------------------------------------------------------------------
 // bitwise OR
-_BIT_ENUM_T inline E operator | (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) | static_cast<T>(b)); }
-_BIT_ENUM_T inline E operator | (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) | b); }
-_BIT_ENUM_T inline E operator |= (E& a, E b) noexcept { return a = a | b; }
-_BIT_ENUM_T inline E operator |= (E& a, T b) noexcept { return a = a | b; }
+_BIT_ENUM_T constexpr E operator | (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) | static_cast<T>(b)); }
+_BIT_ENUM_T constexpr E operator | (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) | b); }
+_BIT_ENUM_T constexpr T operator | (T a, E b) noexcept { return a | static_cast<T>(b); }
+
+_BIT_ENUM_T constexpr E operator |= (E& a, E b) noexcept { return a = a | b; }
+_BIT_ENUM_T constexpr E operator |= (E& a, T b) noexcept { return a = a | b; }
+_BIT_ENUM_T constexpr T operator |= (T& a, E b) noexcept { return a = a | b; }
 
 // bitwise AND
-_BIT_ENUM_T inline E operator & (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) & static_cast<T>(b)); }
-_BIT_ENUM_T inline E operator & (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) & b); }
-_BIT_ENUM_T inline E operator &= (E& a, E b) noexcept { return a = a & b; }
-_BIT_ENUM_T inline E operator &= (E& a, T b) noexcept { return a = a & b; }
+_BIT_ENUM_T constexpr E operator & (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) & static_cast<T>(b)); }
+_BIT_ENUM_T constexpr E operator & (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) & b); }
+_BIT_ENUM_T constexpr T operator & (T a, E b) noexcept { return a & static_cast<T>(b); }
+
+_BIT_ENUM_T constexpr E operator &= (E& a, E b) noexcept { return a = a & b; }
+_BIT_ENUM_T constexpr E operator &= (E& a, T b) noexcept { return a = a & b; }
+_BIT_ENUM_T constexpr E operator &= (T& a, E b) noexcept { return a = a & b; }
 
 // bitwise XOR
-_BIT_ENUM_T inline E operator ^ (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) ^ static_cast<T>(b)); }
-_BIT_ENUM_T inline E operator ^ (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) ^ b); }
-_BIT_ENUM_T inline E operator ^= (E& a, E b) noexcept { return a = a ^ b; }
-_BIT_ENUM_T inline E operator ^= (E& a, T b) noexcept { return a = a ^ b; }
+_BIT_ENUM_T constexpr E operator ^ (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) ^ static_cast<T>(b)); }
+_BIT_ENUM_T constexpr E operator ^ (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) ^ b); }
+_BIT_ENUM_T constexpr T operator ^ (T a, E b) noexcept { return a ^ static_cast<T>(b); }
+
+_BIT_ENUM_T constexpr E operator ^= (E& a, E b) noexcept { return a = a ^ b; }
+_BIT_ENUM_T constexpr E operator ^= (E& a, T b) noexcept { return a = a ^ b; }
+_BIT_ENUM_T constexpr E operator ^= (T& a, E b) noexcept { return a = a ^ b; }
 
 // bitwise NOT
-_BIT_ENUM_T inline E operator ~ (E a) noexcept { return static_cast<E>(~static_cast<T>(a)); }
+_BIT_ENUM_T constexpr E operator ~ (E a) noexcept { return static_cast<E>(~static_cast<T>(a)); }
 
+// shift left
+_ARIT_ENUM_T constexpr E operator << (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) << static_cast<T>(b)); }
+_ARIT_ENUM_T constexpr E operator << (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) << b); }
+_ARIT_ENUM_T constexpr T operator << (T a, E b) noexcept { return a << static_cast<T>(b); }
+
+_ARIT_ENUM_T constexpr E operator <<= (E& a, E b) noexcept { return a = a << b; }
+_ARIT_ENUM_T constexpr E operator <<= (E& a, T b) noexcept { return a = a << b; }
+_ARIT_ENUM_T constexpr T operator <<= (T& a, E b) noexcept { return a = a << b; }
+
+// shift right
+_ARIT_ENUM_T constexpr E operator >> (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) >> static_cast<T>(b)); }
+_ARIT_ENUM_T constexpr E operator >> (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) >> b); }
+_ARIT_ENUM_T constexpr T operator >> (T a, E b) noexcept { return a >> static_cast<T>(b); }
+
+_ARIT_ENUM_T constexpr E operator >>= (E& a, E b) noexcept { return a = a >> b; }
+_ARIT_ENUM_T constexpr E operator >>= (E& a, T b) noexcept { return a = a >> b; }
+_ARIT_ENUM_T constexpr T operator >>= (T& a, E b) noexcept { return a = a >> b; }
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+// Compare operators
+//------------------------------------------------------------------------------------------------------------------------------
 // logical NOT
-_BIT_ENUM_T inline bool operator ! (E a) noexcept { return !static_cast<T>(a); }
+_CMP_ENUM_T constexpr bool operator ! (E a) noexcept { return !static_cast<T>(a); }
 
 // logical EQ
-_BIT_ENUM_T inline bool operator == (E a, T b) noexcept { return static_cast<T>(a) == b; }
+_CMP_ENUM_T constexpr bool operator == (E a, T b) noexcept { return static_cast<T>(a) == b; }
+_CMP_ENUM_T constexpr bool operator == (T a, E b) noexcept { return a == static_cast<T>(b); }
 
 // logical NEQ
-_BIT_ENUM_T inline bool operator != (E a, T b) noexcept { return static_cast<T>(a) != b; }
+_CMP_ENUM_T constexpr bool operator != (E a, T b) noexcept { return !(a == b); }
+_CMP_ENUM_T constexpr bool operator != (T a, E b) noexcept { return !(a == b); }
+
+// logical GT
+_CMP_ENUM_T constexpr bool operator > (E a, T b) noexcept { return static_cast<T>(a) > b; }
+_CMP_ENUM_T constexpr bool operator > (T a, E b) noexcept { return a > static_cast<T>(b); }
+
+// logical LT
+_CMP_ENUM_T constexpr bool operator < (E a, T b) noexcept { return static_cast<T>(a) < b; }
+_CMP_ENUM_T constexpr bool operator < (T a, E b) noexcept { return a < static_cast<T>(b); }
+
+// logical GTE
+_CMP_ENUM_T constexpr bool operator >= (E a, T b) noexcept { return !(a < b); }
+_CMP_ENUM_T constexpr bool operator >= (T a, E b) noexcept { return !(a < b); }
+
+// logical LTE
+_CMP_ENUM_T constexpr bool operator <= (E a, T b) noexcept { return !(a > b); }
+_CMP_ENUM_T constexpr bool operator <= (T a, E b) noexcept { return !(a > b); }
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+// Arithmetic operators
+//------------------------------------------------------------------------------------------------------------------------------
+// negate
+_ARIT_ENUM_T constexpr E operator + (E a) noexcept { return static_cast<E>(+static_cast<T>(a)); }
+_ARIT_ENUM_T constexpr E operator - (E a) noexcept { return static_cast<E>(-static_cast<T>(a)); }
+
+// increment
+_ARIT_ENUM_T constexpr E operator ++ (E& a) noexcept { a = static_cast<E>(static_cast<T>(a) + 1); return a; }
+_ARIT_ENUM_T constexpr E operator ++ (E& a, int) noexcept { E tmp = a; a = static_cast<E>(static_cast<T>(a) + 1); return tmp; }
+
+// decrement
+_ARIT_ENUM_T constexpr E operator -- (E& a) noexcept { a = static_cast<E>(static_cast<T>(a) - 1); return a; }
+_ARIT_ENUM_T constexpr E operator -- (E& a, int) noexcept { E tmp = a; a = static_cast<E>(static_cast<T>(a) - 1); return tmp; }
+
+// add
+_ARIT_ENUM_T constexpr E operator + (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) + static_cast<T>(b)); }
+_ARIT_ENUM_T constexpr E operator + (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) + b); }
+_ARIT_ENUM_T constexpr T operator + (T a, E b) noexcept { return a + static_cast<T>(b); }
+
+_ARIT_ENUM_T constexpr E operator += (E& a, E b) noexcept { return a = a + b; }
+_ARIT_ENUM_T constexpr E operator += (E& a, T b) noexcept { return a = a + b; }
+_ARIT_ENUM_T constexpr T operator += (T& a, E b) noexcept { return a = a + b; }
+
+// subtract
+_ARIT_ENUM_T constexpr E operator - (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) - static_cast<T>(b)); }
+_ARIT_ENUM_T constexpr E operator - (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) - b); }
+_ARIT_ENUM_T constexpr T operator - (T a, E b) noexcept { return a - static_cast<T>(b); }
+
+_ARIT_ENUM_T constexpr E operator -= (E& a, E b) noexcept { return a = a - b; }
+_ARIT_ENUM_T constexpr E operator -= (E& a, T b) noexcept { return a = a - b; }
+_ARIT_ENUM_T constexpr T operator -= (T& a, E b) noexcept { return a = a - b; }
+
+// multiply
+_ARIT_ENUM_T constexpr E operator * (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) * static_cast<T>(b)); }
+_ARIT_ENUM_T constexpr E operator * (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) * b); }
+_ARIT_ENUM_T constexpr T operator * (T a, E b) noexcept { return a * static_cast<T>(b); }
+
+_ARIT_ENUM_T constexpr E operator *= (E& a, E b) noexcept { return a = a * b; }
+_ARIT_ENUM_T constexpr E operator *= (E& a, T b) noexcept { return a = a * b; }
+_ARIT_ENUM_T constexpr T operator *= (T& a, E b) noexcept { return a = a * b; }
+
+// divide
+_ARIT_ENUM_T constexpr E operator / (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) / static_cast<T>(b)); }
+_ARIT_ENUM_T constexpr E operator / (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) / b); }
+_ARIT_ENUM_T constexpr T operator / (T a, E b) noexcept { return a / static_cast<T>(b); }
+
+_ARIT_ENUM_T constexpr E operator /= (E& a, E b) noexcept { return a = a / b; }
+_ARIT_ENUM_T constexpr E operator /= (E& a, T b) noexcept { return a = a / b; }
+_ARIT_ENUM_T constexpr T operator /= (T& a, E b) noexcept { return a = a / b; }
+
+// modulo
+_ARIT_ENUM_T constexpr E operator % (E a, E b) noexcept { return static_cast<E>(static_cast<T>(a) % static_cast<T>(b)); }
+_ARIT_ENUM_T constexpr E operator % (E a, T b) noexcept { return static_cast<E>(static_cast<T>(a) % b); }
+_ARIT_ENUM_T constexpr T operator % (T a, E b) noexcept { return a % static_cast<T>(b); }
+
+_ARIT_ENUM_T constexpr E operator %= (E& a, E b) noexcept { return a = a % b; }
+_ARIT_ENUM_T constexpr E operator %= (E& a, T b) noexcept { return a = a % b; }
+_ARIT_ENUM_T constexpr T operator %= (T& a, E b) noexcept { return a = a % b; }
+
